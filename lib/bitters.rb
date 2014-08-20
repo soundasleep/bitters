5ddb0e339e041f512b2e23b99df114edf5201946
Jevon Wright <jevon@jevon.org>
2014-08-20 17:07:44 +1200

    Register bitters path as a SASS include path

--- lib/bitters.rb
+++ lib/bitters.rb
@@ -1,6 +1,22 @@
 require "bitters/version"
 require "bitters/generator"
 
+# Copied from Bourbon.rb
 module Bitters
-  # Your code goes here...
+  if defined?(Rails) && defined?(Rails::Engine)
+    class Engine < ::Rails::Engine
+      require 'bourbon/engine'
+    end
+
+    # module Rails
+    #   class Railtie < ::Rails::Railtie
+    #     rake_tasks do
+    #       load "tasks/install.rake"
+    #     end
+    #   end
+    # end
+  else
+    bitters_path = File.expand_path("../../app/assets/stylesheets", __FILE__)
+    ENV["SASS_PATH"] = [ENV["SASS_PATH"], bitters_path].compact.join(File::PATH_SEPARATOR)
+  end
 end
